d58632fcdab27a2a8df8d8f7546f457ccc75fa24
ReGYChang <32244113+ReGYChang@users.noreply.github.com>
2017-10-30 22:41:23 +0800

    just do it
    done

--- HW2.java
+++ HW2.java
@@ -1,9 +1,12 @@
+package homework;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  * @author TODO: please add student ID and name here
+ * B0544122 張家槃
  * Try to write some comments for your codes (methods, 15 points)
  */
 public class HW2 {
@@ -26,8 +29,8 @@ public class HW2 {
 	}
 	/**
 	 * This method is used for checking your result, not a part of your HW2
-	 * @param allCards 所有的牌
-	 * @param nDeck 總共有幾副牌
+	 * @param allCards �Ҧ����P
+	 * @param nDeck �`�@���X�ƵP
 	 * @return
 	 */
 	private static boolean isAllCardsCorrect(ArrayList<Card> allCards,int nDeck){
@@ -65,12 +68,25 @@ public class HW2 {
 }
 /**
  * Description: TODO: please add description here
+ * 餵給類別Deck有n副牌，將n副牌中的Card一一存進陣列清單cards
+ * 呼叫陣列清單cards中的card並使用其方法printCard將卡牌印出來
  */
 class Deck{
 	private ArrayList<Card> cards;
 	//TODO: Please implement the constructor (30 points)
 	public Deck(int nDeck){
 		cards=new ArrayList<Card>();
+		for (int i = 0; i < nDeck; i ++)
+		{
+			for(int j = 1; j <= 4; j ++)
+			{
+				for(int k = 1; k <= 13; k++)
+				{
+					Card card = new Card(j,k);
+					cards.add(card);
+				}
+			}
+		}
 		//1 Deck have 52 cards, https://en.wikipedia.org/wiki/Poker
 		//Hint: Use new Card(x,y) and 3 for loops to add card into deck
 		//Sample code start
@@ -83,7 +99,10 @@ class Deck{
 	public void printDeck(){
 		//Hint: print all items in ArrayList<Card> cards, 
 		//TODO: please implement and reuse printCard method in Card class (5 points)
-
+		for (int i = 0; i < cards.size(); i ++)
+		{
+			cards.get(i).printCard();
+		}
 	}
 	public ArrayList<Card> getAllCards(){
 		return cards;
@@ -91,6 +110,7 @@ class Deck{
 }
 /**
  * Description: TODO: please add description here
+ * 方法printCard將定義類別Card花色的數字轉換成文字
  */
 class Card{
 	private int suit; //Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3, Spades=4
@@ -107,7 +127,29 @@ class Card{
 	public void printCard(){
 		//Hint: print (System.out.println) card as suit,rank, for example: print 1,1 as Clubs Ace
 
-	}
+		if (suit == 1  )	
+			if (rank == 1)
+				System.out.println("Clubs,"+"Ace");
+			else
+				System.out.println("Clubs,"+rank);
+		else if(suit == 2  )
+			if (rank == 1)
+				System.out.println("Diamonds,"+"Ace");
+			else
+				System.out.println("Diamonds,"+rank);
+		else if(suit == 3  )
+			if (rank == 1)
+				System.out.println("Hearts,"+"Ace");
+			else
+				System.out.println("Hearts,"+rank);
+		else 
+			if (rank == 1)
+				System.out.println("Spades,"+"Ace");
+			else
+				System.out.println("Spades,"+rank);
+	
+		}
+
 	public int getSuit(){
 		return suit;
 	}
